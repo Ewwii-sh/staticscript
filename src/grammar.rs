11c413d1237313d08987e00a609de2cfae6fc3a2
Byson94 <byson94@outlook.com>
2025-08-14 22:22:44 +0530

    added gh-pages branch for deployment

--- src/grammar.rs
+++
@@ -1,6 +0,0 @@
-use pest_derive::Parser;
-
-// This tells Rust to use `grammar.pest` as the grammar
-#[derive(Parser)]
-#[grammar = "grammar.pest"]
-pub struct DslParser;
--- src/transpiler/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod core;
-pub mod containers;
-pub mod props_only;
-pub mod utils;
-
-pub use core::transpile_code;
